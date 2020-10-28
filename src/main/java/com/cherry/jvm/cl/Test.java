01c999196dada7087512ffccf5c157263c6adaaf
wu383933588lyy <49613046+wu383933588lyy@users.noreply.github.com>
2020-10-28 14:55:01 +0800

    新增

--- src/main/java/com/cherry/jvm/cl/Test.java
+++ src/main/java/com/cherry/jvm/cl/Test.java
@@ -10,6 +10,15 @@ package com.cherry.jvm.cl;
 public class Test {
 
     public static void main(String[] args) {
+        // BootStrapClassLoader
+        // 加载基本类 Object Class rt.jar
+        System.out.println(System.getProperty("sun.boot.class.path"));
+        // ExtClassLoader
+        // 加载扩展类 ext 目录中
+        System.out.println(System.getProperty("java.class.path"));
+        // AppClassLoader
+        // 加载工程路径中的 Class
+        System.out.println(System.getProperty("java.ext.dirs"));
         ClassLoader loader = Test.class.getClassLoader();
         while (loader != null) {
             System.out.println(loader);
